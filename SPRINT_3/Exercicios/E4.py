3417d0310a6ee67f1694c59f634378c42db5d211
Victor Hugo <victorhridnare@gmail.com>
2023-07-19 15:24:32 -0400

    correção de exercícios

--- SPRINT_3/Exercicios/E4.py
+++ SPRINT_3/Exercicios/E4.py
@@ -1,4 +1,26 @@
-um = 1
-for i in range(1, 100):
-    if i / i == um and i / um == i:
-        print(i)
+# Primeiro se cria a função que irá verificar se um número é primo ou não
+def is_prime(number):
+    if number < 2:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+# Depois criamos uma lista para armazenas os numeros primos
+
+primos = []
+
+# Por fim, utilizamos um laço para percorrer os numeros de 1 a 100 e chamando
+# a função is_prime passando num como parâmetro para a verificação que será
+# feita pela função.
+
+for num in range(1, 101):
+    if is_prime(num):
+        primos.append(num)
+
+
+print(primos) #para imprimir a lista inteira
+
+for i in primos: #para imprimir os items um por um
+    print(i)
